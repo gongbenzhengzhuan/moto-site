f0ae91ae669424af0cc4c5c5180451f80f3fef4e
gongbenzheng <1459490255@qq.com>
2023-06-12 11:23:45 +0800

    页貌调整-英语格式

--- moto-client/src/views/client/EnglishView.vue
+++ moto-client/src/views/client/EnglishView.vue
@@ -1,22 +1,27 @@
 <template>
-<div>日常视频
-  <br/><br/>
-  <a href="https://haokan.baidu.com/v?vid=5822979698691391583" target="_blank" rel="nexus">奥巴马2008年就职演讲</a>
-  <br/><br/>
-  <a href="https://www.ixigua.com/6789919411642827272" target="_blank" rel="nexus">奥巴马最后一场白宫演讲</a>
-  <br/><br/>
-  <a href="https://tv.360kan.com/player?id=eab57a7b8117d9a48e1527ec6dff6ddf&q=%E5%A5%A5%E5%B7%B4%E9%A9%AC%E6%BC%94%E8%AE%B2&src=result-like&srcg=pc_daohang&sid=d954d4ac51284ebcb2991218ae78640f" target="_blank" rel="nexus">奥巴马无畏的希望</a>
-  <br/><br/>
-  <a href="http://www.iqiyi.com/w_19rsjdiw15.html" target="_blank" rel="nexus">奥巴马复旦演讲</a>
-</div>
-  <br/><br/>
-<div>电影</div>
-  <br/><br/>
-  <a href="https://v.youku.com/v_nextstage/id_cc00e332962411de83b1.html?spm=a2hja.12701310.filter.454&s=cc00e332962411de83b1" target="_blank" rel="nexus">雌雄大盗</a>
-  <br/><br/>
-  <a href="https://v.youku.com/v_nextstage/id_cc170b1c962411de83b1.html?spm=a2hja.12701310.filter.27&s=cc170b1c962411de83b1" target="_blank" rel="nexus">生活大爆炸</a>
-  <br/><br/>
-<div>听力书</div>
+  <div id="look">
+    <div>日常视频
+      <br/><br/>
+      <a href="https://haokan.baidu.com/v?vid=5822979698691391583" target="_blank" rel="nexus">奥巴马2008年就职演讲</a>
+      <br/><br/>
+      <a href="https://www.ixigua.com/6789919411642827272" target="_blank" rel="nexus">奥巴马最后一场白宫演讲</a>
+      <br/><br/>
+      <a href="https://tv.360kan.com/player?id=eab57a7b8117d9a48e1527ec6dff6ddf&q=%E5%A5%A5%E5%B7%B4%E9%A9%AC%E6%BC%94%E8%AE%B2&src=result-like&srcg=pc_daohang&sid=d954d4ac51284ebcb2991218ae78640f"
+         target="_blank" rel="nexus">奥巴马无畏的希望</a>
+      <br/><br/>
+      <a href="http://www.iqiyi.com/w_19rsjdiw15.html" target="_blank" rel="nexus">奥巴马复旦演讲</a>
+    </div>
+    <br/><br/>
+    <div>电影</div>
+    <br/><br/>
+    <a href="https://v.youku.com/v_nextstage/id_cc00e332962411de83b1.html?spm=a2hja.12701310.filter.454&s=cc00e332962411de83b1"
+       target="_blank" rel="nexus">雌雄大盗</a>
+    <br/><br/>
+    <a href="https://v.youku.com/v_nextstage/id_cc170b1c962411de83b1.html?spm=a2hja.12701310.filter.27&s=cc170b1c962411de83b1"
+       target="_blank" rel="nexus">生活大爆炸</a>
+    <br/><br/>
+    <div>听力书</div>
+  </div>
 </template>
 
 <script>
@@ -26,5 +31,7 @@ export default {
 </script>
 
 <style scoped>
-
+#look {
+  text-align: center;
+}
 </style>
